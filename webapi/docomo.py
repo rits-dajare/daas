cbcdf89a4c553c773b06da0dd89a6d50631c9af4
AjxLab <abe12@mccc.jp>
2020-07-09 00:09:37 +0900

    api by jetrun

--- webapi/docomo.py
+++ webapi/docomo.py
@@ -20,9 +20,37 @@ goo_api = http_request.HTTP()
 jetrun_api = http_request.HTTP()
 
 
+def check_health(res, alert=True):
+    code = res.status_code
+    try:
+        assert code == requests.codes.ok
+    except:
+        url = 'https://notify-api.line.me/api/notify'
+        header = {'Authorization': 'Bearer ' + LINE}
+
+        try:
+            # send message from LINE
+            if alert:
+                message = open('config/alert.txt', 'r').read()
+                timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                message = message.replace('{timestamp}', timestamp)
+                message = message.replace('{code}', str(code))
+                message = message.replace('{url}', res.url)
+                message = message.replace('{json}', str(res.json()))
+
+                param = {'message': message}
+                requests.post(url, headers=header, params=param)
+        except:
+            pass
+
+        return False
+
+    return True
+
+
 def to_reading(dajare):
     url = 'https://api.apigw.smt.docomo.ne.jp/gooLanguageAnalysis/v1/hiragana?APIKEY={}'
-    headers = { 'Content-Type': 'application/json' }
+    headers = {'Content-Type': 'application/json'}
     params = {'sentence': dajare, 'output_type': 'katakana'}
 
     for key in APIKEY:
@@ -31,8 +59,25 @@ def to_reading(dajare):
             headers,
             json.dumps(params)
         )
+        if check_health(res, False):
+            return res
 
-        return res
+    return res
 
 
-print(to_reading('布団が吹っ飛んだ').json())
+def find_sensitive_tags(dajare):
+    url = 'https://api.apigw.smt.docomo.ne.jp/truetext/v1/sensitivecheck?APIKEY={}'
+    headers = { 'Content-Type': 'application/x-www-form-urlencoded' }
+    params = { 'text': dajare }
+
+    for key in APIKEY:
+        jetrun_api.set_url(url.format(key))
+        res = jetrun_api.post(
+            headers,
+            params
+        )
+        if check_health(res, False):
+            return res
+
+    return res
+
