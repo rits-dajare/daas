5937d65e9b46ce9ee43ed13063a250f8523187ef
AjxLab <abe12@mccc.jp>
2020-07-09 20:32:22 +0900

    finish to develop judge engine

--- engine/judge_engine.py
+++ engine/judge_engine.py
@@ -20,8 +20,72 @@ class JudgeEngine(engine.Engine):
         self.tokenize = Tokenizer()
 
     def is_dajare(self, dajare):
+        # convert dajare to reading & morphemes
+        reading, morphemes = self.to_reading_and_morphemes(dajare)
+        reading, morphemes = self.preprocessing(reading, morphemes)
 
-        return True
+        # exclude 'ッ'
+        reading_excluded_tu = reading.replace('ッ', '')
+        morphemes_excluded_tu = [m.replace('ッ', '') for m in morphemes]
+
+        if self.judge(reading, morphemes):
+            return True
+        if self.judge(reading_excluded_tu, morphemes_excluded_tu):
+            return True
+
+        return False
+
+    def judge(self, reading, morphemes):
+        # whether morpheme is included multiple
+        for m in morphemes:
+            if reading.count(m) >= 2:
+                return True
+
+        # whether judgment rules holds
+        tri_char = self.n_gram(reading, 3)
+
+        for i, tri1 in enumerate(tri_char):
+            for tri2 in tri_char[(i+1):]:
+                # all match
+                if tri1 == tri2:
+                    return True
+
+                # 2 chars match && all vowels match
+                if self.count_str_match(tri1, tri2) == 2:
+                    if pyboin.text2boin(tri1) == pyboin.text2boin(tri2):
+                        return True
+
+        if 'ー' in reading:
+            # exclude 'ー'
+            if self.judge(reading.replace('ー', ''), []):
+                return True
+
+            # convert 'ー' to last vowel
+            # ex. 'カー' -> 'カア'
+            hyphen_to_vowel_reading = ''
+            patterns = re.findall(r'.ー', reading)
+            for ch in patterns:
+                hyphen_to_vowel_reading = reading.replace(
+                    ch,
+                    ch[0] + pyboin.text2boin(ch[0])
+                )
+            if self.judge(hyphen_to_vowel_reading, []):
+                return True
+
+        # convert char to next lower's vowel
+        # ex. 'シュン' -> 'ウン'
+        matches = re.findall(r'.[ァィゥェォャュョヮ]', reading)
+        if matches != []:
+            lower_to_vowel_reading = reading
+            for ch in matches:
+                lower_to_vowel_reading = lower_to_vowel_reading.replace(
+                    ch,
+                    pyboin.text2boin(ch[1])
+                )
+            if self.judge(lower_to_vowel_reading, []):
+                return True
+
+        return False
 
     def to_reading_and_morphemes(self, dajare, use_api=True):
         reading = ''
@@ -60,13 +124,22 @@ class JudgeEngine(engine.Engine):
 
         return reading, morphemes
 
-    def preprocessing(self, dajare):
-        # excludes chars that are consecutive 3~ times
-        dajare = re.sub(r'(.)\1{2,}', r'\1', dajare)
+    def preprocessing(self, reading, morphemes):
+        # nomalize text
+        nomalize_pair = [
+            'ヂガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポ',
+            'ジカキクケコサシスセソタチツテトハヒフヘホハヒフヘホ'
+        ]
+        for i in range(len(nomalize_pair[0])):
+            reading = reading.replace(
+                nomalize_pair[0][i],
+                nomalize_pair[1][i],
+            )
+            morphemes = [m.replace(
+                nomalize_pair[0][i],
+                nomalize_pair[1][i]) for m in morphemes]
 
-        # convert dajare to reading & morphemes
-        reading, morphemes = self.to_reading_and_morphemes(dajare)
-
+        # convert looped vowel text to '-'
         for ci in range(len(reading) - 1):
             if reading[ci+1] not in 'アイウエオ':
                 continue
@@ -83,13 +156,28 @@ class JudgeEngine(engine.Engine):
         for bi_char in self.n_gram(reading, 2):
             for sub in vowel_pattern:
                 if pyboin.text2boin(bi_char) == sub[0]:
-                    reading = reading.replace(*sub)
+                    reading = reading.replace(bi_char, sub[1])
 
         return reading, morphemes
 
     def n_gram(self, dajare, n):
         return [dajare[idx:idx + n] for idx in range(len(dajare) - n + 1)]
 
+    def count_str_match(self, s1, s2):
+        count = 0
+        for i in range(len(s1)):
+            if s1[i] == s2[i%len(s2)]:
+                count += 1
+
+        return count
+
 
 judge_engine = JudgeEngine()
-print(judge_engine.preprocessing('紅茶が凍っちゃった'))
+print(judge_engine.is_dajare('布団が吹っ飛んだ'))
+print(judge_engine.is_dajare('紅茶が凍っちゃった'))
+print(judge_engine.is_dajare('ローソンのローション'))
+print(judge_engine.is_dajare('初見の食券'))
+print(judge_engine.is_dajare('芸無なゲーム'))
+print(judge_engine.is_dajare('Gmailで爺滅入る'))
+print(judge_engine.is_dajare('スロットで金すろーと'))
+print(judge_engine.is_dajare('ニューヨークで入浴'))
