ba30e80b2c5a722af35e94aaa314612ffc645889
averak <abe12@mccc.jp>
2020-08-09 12:38:07 +0900

    [test] 英単語の読み変換をテスト

--- tests/test_reading.py
+++ tests/test_reading.py
@@ -13,3 +13,16 @@ class TestReading(unittest.TestCase):
         self.assertEqual('コンニチハ', self.converter.convert(text, True))
         # without api
         self.assertEqual('コンニチハ', self.converter.convert(text, False))
+
+    def test_convert_eng_word_to_reading(self):
+        texts = [
+            ['エービーシーディー', 'ABCD'],
+            ['エービーシー', 'ABC'],
+            ['', 'abcd'],
+            ['ハロー', 'hello'],
+        ]
+        for text in texts:
+            self.assertEqual(
+                text[0],
+                self.converter.convert(text[1])
+            )
