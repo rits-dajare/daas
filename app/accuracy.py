84159e4f1fbd46d32683ced7588476f158ff8936
averak <abe12@mccc.jp>
2021-02-21 11:26:40 +0900

    [feat] 計測ツールにコマンドライン引数を与える

--- app/accuracy.py
+++ app/accuracy.py
@@ -4,51 +4,77 @@
 ダジャレ判定AIの精度評価
 '''
 
+import sys
 import json
 import glob
 import tqdm
-import random
+import numpy as np
 import engine
+import argparse
 
-enable_judge = True
-enable_eval = True
 
-# データセットを作成
-data = []
-for file in glob.glob('data/*.json'):
-    with open(file) as f:
-        data.extend(json.load(f))
-if len(data) == 0:
-    exit(0)
+def prepare_data(num=None, shuffle=True):
+    result = []
 
-random.shuffle(data)
-data = data[:300]
+    for file in glob.glob('data/*.json'):
+        with open(file, 'r') as f:
+            result.extend(json.load(f))
 
-if enable_judge:
-    print('判定AIの評価...')
+    if shuffle:
+        np.random.shuffle(result)
+    if num is not None:
+        result = result[:num]
+
+    return result
+
+
+def measure_judge_engine(data):
     n_correct = 0
     for row in tqdm.tqdm(data):
         try:
             if row['is_joke'] == \
                     engine.judge_engine.execute(row['joke'], False):
                 n_correct += 1
-            else:
-                pass
         except Exception as e:
-            print(row['joke'])
+            print('エラー発生：', row['joke'])
             print(e)
+            sys.exit(1)
 
-    print('精度：%f' % (n_correct / len(data)))
+    return '判定精度：%f' % (n_correct / len(data))
 
-if enable_eval:
-    print('評価AIの評価...')
+
+def measure_eval_engine(data):
     score_map = [0, 0, 0, 0, 0]
 
-    a = []
     for row in tqdm.tqdm(data):
         score = engine.eval_engine.execute(row['joke'], False)
-        a.append(score)
         score = int(round(score))
         score_map[score - 1] += 1
 
-print(score_map)
+    result = ''
+    for i, n in enumerate(score_map):
+        result += ' - 星%d：%3.1f%%\n' % (i + 1, n / len(data) * 100)
+
+    return result
+
+
+if __name__ == '__main__':
+    # opts conf
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-j', '--judge', help='measure the judge accuracy',
+                        action='store_true')
+    parser.add_argument('-e', '--eval', help='measure the eval accuracy',
+                        action='store_true')
+    parser.add_argument('-n', '--num', help='number to measure',
+                        type=int)
+    args = parser.parse_args()
+
+    # 検証用データを用意
+    data = prepare_data(args.num)
+
+    if args.judge:
+        print('判定AIの評価...')
+        print(measure_judge_engine(data))
+    if args.eval:
+        print('評価AIの評価...')
+        print(measure_eval_engine(data))
