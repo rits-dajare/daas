357ec4315f87a43fc3789e23801212562b3f153e
averak <abe12@mccc.jp>
2021-04-04 00:43:28 +0900

    [refactor] delete nnet

--- app/engine/eval_engine.py
+++ app/engine/eval_engine.py
@@ -1,7 +1,6 @@
 from functools import lru_cache
 import numpy as np
 from .engine import Engine
-from .nnet.cnn import CNN
 
 
 class EvalEngine(Engine):
@@ -9,7 +8,6 @@ class EvalEngine(Engine):
         self.score_cache = []
 
         self.__max_length = 100
-        self.__nnet = CNN()
 
     @lru_cache(maxsize=255)
     def execute(self, text, use_api=True):
@@ -42,37 +40,3 @@ class EvalEngine(Engine):
         if result < 1.0:
             return 1.0 + np.random.rand() / 5
         return result
-
-        # pred = self.nnet.predict(np.array([vec]))
-        # result = abs((pred[0] - 0.5638) / 0.02292)
-        # result = result * 4.0 + 1.0
-
-        # while result < 1.0 or result > 5.0:
-        #     if result < 1.0:
-        #         result += 2.0
-        #     if result > 5.0:
-        #         result -= 2.5
-
-        # return result
-
-    def train(self, data):
-        # data: [text:str, score:float]
-        import tqdm
-
-        x = []
-        y = []
-        print('データセットを作成...')
-        for row in tqdm.tqdm(data):
-            katakana = self.text_service.katakanize(row[0], False)
-            x.append(self.text_service.conv_vector(
-                katakana, self.__max_length))
-            y.append(row[1] / 5.0)
-
-        self.nnet.train(
-            np.array(x),
-            np.array(y),
-        )
-
-    @property
-    def nnet(self):
-        return self.__nnet
