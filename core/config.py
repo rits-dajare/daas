27626a911c98d00fbfad3f168f2d6768a390ee66
averak <abe12@mccc.jp>
2021-04-04 09:06:58 +0900

    [feat] debug mode -> True

--- core/config.py
+++ core/config.py
@@ -15,4 +15,4 @@ API_SUCCESS_MSG: str = 'success'
 API_RESPONSE: dict = {'message': API_SUCCESS_MSG}
 API_HOST: str = '0.0.0.0'
 API_PORT: int = 50000
-API_DEBUG: bool = False
+API_DEBUG: bool = True
