4c39d5f773262851928255447e8ae309c077fb18
averak <abe12@mccc.jp>
2021-05-20 12:26:22 +0900

    [test] APIのレスポンスをテスト

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 
 
 class TestAPI(unittest.TestCase):
@@ -8,7 +9,12 @@ class TestAPI(unittest.TestCase):
 
     def test_正_ダジャレを判定(self):
         res = self.app.get('judge/', query_string={'dajare': '布団が吹っ飛んだ'})
+        res_json: dict = json.loads(res.data)
+
         self.assertEqual(200, res.status_code)
+        self.assertEqual("OK", res_json["status"])
+        self.assertEqual("success", res_json["message"])
+        self.assertIsInstance(res_json["is_dajare"], bool)
 
     def test_異_判定APIのパラメータが不足(self):
         res = self.app.get('judge/', query_string={})
@@ -16,7 +22,12 @@ class TestAPI(unittest.TestCase):
 
     def test_正_ダジャレを評価(self):
         res = self.app.get('eval/', query_string={'dajare': '布団が吹っ飛んだ'})
+        res_json: dict = json.loads(res.data)
+
         self.assertEqual(200, res.status_code)
+        self.assertEqual("OK", res_json["status"])
+        self.assertEqual("success", res_json["message"])
+        self.assertIsInstance(res_json["score"], float)
 
     def test_異_評価APIのパラメータが不足(self):
         res = self.app.get('eval/', query_string={})
@@ -24,7 +35,12 @@ class TestAPI(unittest.TestCase):
 
     def test_正_ダジャレを読みに変換(self):
         res = self.app.get('reading/', query_string={'dajare': '布団が吹っ飛んだ'})
+        res_json: dict = json.loads(res.data)
+
         self.assertEqual(200, res.status_code)
+        self.assertEqual("OK", res_json["status"])
+        self.assertEqual("success", res_json["message"])
+        self.assertIsInstance(res_json["reading"], str)
 
     def test_異_読み変換APIのパラメータが不足(self):
         res = self.app.get('reading/', query_string={})
