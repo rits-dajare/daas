9e7e9ce86d732b109aaa18d37591ae24c1240fff
averak <abe12@mccc.jp>
2021-05-20 17:57:50 +0900

    [test] remove traling slash

--- tests/test_api.py
+++ tests/test_api.py
@@ -9,7 +9,7 @@ class TestAPI(unittest.TestCase):
         self.app = create_app().test_client()
 
     def test_正_ダジャレを判定(self):
-        res = self.app.get('judge/', query_string={'dajare': '布団が吹っ飛んだ'})
+        res = self.app.get('judge', query_string={'dajare': '布団が吹っ飛んだ'})
         res_json: dict = json.loads(res.data)
 
         self.assertEqual(200, res.status_code)
@@ -18,11 +18,11 @@ class TestAPI(unittest.TestCase):
         self.assertIsInstance(res_json["is_dajare"], bool)
 
     def test_異_判定APIのパラメータが不足(self):
-        res = self.app.get('judge/', query_string={})
+        res = self.app.get('judge', query_string={})
         self.assertEqual(400, res.status_code)
 
     def test_正_ダジャレを評価(self):
-        res = self.app.get('eval/', query_string={'dajare': '布団が吹っ飛んだ'})
+        res = self.app.get('eval', query_string={'dajare': '布団が吹っ飛んだ'})
         res_json: dict = json.loads(res.data)
 
         self.assertEqual(200, res.status_code)
@@ -31,11 +31,11 @@ class TestAPI(unittest.TestCase):
         self.assertIsInstance(res_json["score"], float)
 
     def test_異_評価APIのパラメータが不足(self):
-        res = self.app.get('eval/', query_string={})
+        res = self.app.get('eval', query_string={})
         self.assertEqual(400, res.status_code)
 
     def test_正_ダジャレを読みに変換(self):
-        res = self.app.get('reading/', query_string={'dajare': '布団が吹っ飛んだ'})
+        res = self.app.get('reading', query_string={'dajare': '布団が吹っ飛んだ'})
         res_json: dict = json.loads(res.data)
 
         self.assertEqual(200, res.status_code)
@@ -44,5 +44,5 @@ class TestAPI(unittest.TestCase):
         self.assertIsInstance(res_json["reading"], str)
 
     def test_異_読み変換APIのパラメータが不足(self):
-        res = self.app.get('reading/', query_string={})
+        res = self.app.get('reading', query_string={})
         self.assertEqual(400, res.status_code)
