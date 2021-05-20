e6f93af36c8ce398000c1e25116d63f591e3a528
averak <abe12@mccc.jp>
2021-05-20 11:44:25 +0900

    [test] APIの統合テスト

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,14 +6,26 @@ class TestAPI(unittest.TestCase):
         from core.api import controller
         self.app = controller.create_app().test_client()
 
-    def test_judge(self):
+    def test_正_ダジャレを判定(self):
         res = self.app.get('judge/', query_string={'dajare': '布団が吹っ飛んだ'})
         self.assertEqual(200, res.status_code)
 
-    def test_eval(self):
+    def test_異_判定APIのパラメータが不足(self):
+        res = self.app.get('judge/', query_string={})
+        self.assertEqual(400, res.status_code)
+
+    def test_正_ダジャレを評価(self):
         res = self.app.get('eval/', query_string={'dajare': '布団が吹っ飛んだ'})
         self.assertEqual(200, res.status_code)
 
-    def test_reading(self):
+    def test_異_評価APIのパラメータが不足(self):
+        res = self.app.get('eval/', query_string={})
+        self.assertEqual(400, res.status_code)
+
+    def test_正_ダジャレを読みに変換(self):
         res = self.app.get('reading/', query_string={'dajare': '布団が吹っ飛んだ'})
         self.assertEqual(200, res.status_code)
+
+    def test_異_読み変換APIのパラメータが不足(self):
+        res = self.app.get('reading/', query_string={})
+        self.assertEqual(400, res.status_code)
