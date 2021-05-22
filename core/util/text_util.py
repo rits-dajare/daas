8a607d51ce25dc8f61795ff56ce0af8ef7dc44d0
ygkn <2000ygkn0713@gmail.com>
2021-05-23 08:23:36 +0900

    remove unused pattern in regex

--- core/util/text_util.py
+++ core/util/text_util.py
@@ -60,7 +60,7 @@ def preprocessing(text: str) -> str:
     result = mojimoji.zen_to_han(result, kana=False)
     # remove '笑'
     result = re.sub(
-        r'[a-zａ-ｚ]+',
+        r'[a-z]+',
         lambda m:
             '' if re.match(r'^[w]+$', m.group(0), re.IGNORECASE) else m.group(0),
         result,
